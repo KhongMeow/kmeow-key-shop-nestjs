a37dfbbd56259b36280abf4963bbdd390dbca4e9
khongmeow <khongmeow@gmail.com>
2025-03-12 18:47:11 +0700

    update

--- src/balances/balances.module.ts
+++ src/balances/balances.module.ts
@@ -4,9 +4,18 @@ import { BalancesController } from './balances.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Balance } from './entities/balance.entity';
 import { User } from 'src/users/entities/user.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule } from '@nestjs/config';
+import jwtConfig from 'src/identity/config/jwt.config';
+import { RolesModule } from 'src/roles/roles.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Balance, User])],
+  imports: 
+      [TypeOrmModule.forFeature([Balance, User]),
+      JwtModule.registerAsync(jwtConfig.asProvider()),
+      ConfigModule.forFeature(jwtConfig),
+      RolesModule
+    ],
   controllers: [BalancesController],
   providers: [BalancesService],
   exports: [BalancesService],
