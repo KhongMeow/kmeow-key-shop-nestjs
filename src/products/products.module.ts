c02a8f147bf7ea606d538a3fee7fae9005f30ac9
khongmeow <khongmeow@gmail.com>
2025-05-26 21:57:14 +0700

    update

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -8,11 +8,13 @@ import { JwtModule } from '@nestjs/jwt';
 import jwtConfig from 'src/identity/config/jwt.config';
 import { RolesModule } from 'src/roles/roles.module';
 import { CategoriesModule } from 'src/categories/categories.module';
+import { GlobalModule } from 'src/global/global.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Product]),
     CategoriesModule,
+    GlobalModule,
   ],
   controllers: [ProductsController],
   providers: [ProductsService],
