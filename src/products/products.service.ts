9d536397dc48d4101750c1f272ef7de980e7b152
khongmeow <khongmeow@gmail.com>
2025-06-28 20:25:54 +0700

    fix get all products

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -62,7 +62,7 @@ export class ProductsService {
         qb.where('product.category = :categoryId', { categoryId: category.id });
       }
 
-      if (hideSoldOut) {
+      if (hideSoldOut && hideSoldOut === true) {
         qb.having('COUNT(licenseKey.id) > 0');
       }
       
